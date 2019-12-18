694c8ce28d615bafcb9bd6ce07c1a874e2832ef8
Albert <grandia3@hotmail.com>
2019-12-19 10:32:49 +1100

    added 0 for meteorite with no mass

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import meteorRoutes from './routes/meteorRoutes';
-
-
+import cors from 'cors';
 
 const app = express();
+//TODO remove cors, as this is for easier testing purpose only
+app.use(cors());
 app.use(meteorRoutes);
 
 app.listen(8080);
--- src/data/meteorites.ts
+++ src/data/meteorites.ts
@@ -4,10 +4,14 @@ import path from 'path';
 let data = fs.readFileSync(path.join(__dirname, 'nasa-meteorites.json'), 'utf8');
 let jsonData = JSON.parse(data);
 
-// parse the year for easier filtering
 jsonData.forEach((meteorite:any) => {
+    // parse the year for easier filtering
     if (meteorite['year']) {
         meteorite['yearOnly'] = parseInt(meteorite['year'].substring(0,4));
+    }
+    // set mass of meteorite to zero when it has no mass
+    if (!meteorite['mass']) {
+        meteorite['mass'] = 0;
     }
 });
 
