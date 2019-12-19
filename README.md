03dadf6fd5f9a34f7dba09ed5ac409363358c29d
Albert <grandia3@hotmail.com>
2019-12-19 12:10:49 +1100

    added serving of react app files to express

--- README.md
+++ README.md
@@ -7,3 +7,26 @@ Clone this repository
 ```
 git clone https://github.com/albertsundjaja/zwift_coding_challenge
 ```
+
+Do NPM install for this project and its submodule, and build the react web app submodule
+
+```
+npm install
+cd zwift_coding_challenge_react
+npm install
+npm run build
+cd ..
+```
+
+Run the node server
+
+```
+npm run start
+```
+
+Open your browser and navigate to
+
+```
+http://localhost:8080
+```
+
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import meteorRoutes from './routes/meteorRoutes';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 //TODO remove cors, as this is for easier testing purpose only
 app.use(cors());
+
+console.log(path.join(__dirname, '../', 'zwift_coding_challenge/build'));
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../', 'zwift_coding_challenge_react/build')));
+app.get("/", (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'zwift_coding_challenge_react/build', 'index.html'));
+});
+
 app.use(meteorRoutes);
 
 app.listen(8080);
