49da50ae5c884c27e5e9aecef3466bba1515cdda
Albert <grandia3@hotmail.com>
2020-01-04 11:48:48 +1100

    added vue submodule

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,19 @@ const app = express();
 //TODO remove cors, as this is for easier testing purpose only
 app.use(cors());
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../', 'zwift_coding_challenge_react/build')));
-app.get("/react", (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'zwift_coding_challenge_react/build', 'index.html'));
-});
+// Serve the static files from the React or Vue app
+if (process.env.FRONTEND === 'react') {
+  app.use(express.static(path.join(__dirname, '../', 'zwift_coding_challenge_react/build')));
+  app.get("/", (req, res, next) => {
+      res.sendFile(path.join(__dirname, '../', 'zwift_coding_challenge_react/build', 'index.html'));
+  });
+}
+else {
+  app.use(express.static(path.join(__dirname, '../', 'zwift_coding_challenge_vue/dist')));
+  app.get("/", (req, res, next) => {
+      res.sendFile(path.join(__dirname, '../', 'zwift_coding_challenge_vue/dist', 'index.html'));
+  });
+}
 
 app.use(meteorRoutes);
 
